cbf5561e0c6336181cbb07d47ddbad2575bb5870
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-09 22:41:51 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DemoService } from '../demo.service';
 import { Subscription } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-playground',
@@ -15,38 +16,22 @@ export class PlaygroundComponent implements OnInit, OnDestroy {
   constructor(private demoService: DemoService) {}
 
   ngOnInit(): void {
-    this.demoSubscription1 = this.demoService.person$.subscribe(
-      (data) => console.log('Data 1', data),
-      (error) => console.error('Error 1', error),
-      () => console.log('Completed 1')
-    );
-
-    // second subscriber
-    this.demoSubscription2 = this.demoService.person$.subscribe(
-      (data) => console.log('Data 2', data),
-      (error) => console.error('Error 1', error),
-      () => console.log('Completed 1')
-    );
-
-    //third
-    this.demoSubscription3 = this.demoService.person$.subscribe(
-      (data) => console.log('Data 3', data),
-      (error) => console.error('Error 1', error),
-      () => console.log('Completed 1')
-    );
+    this.demoSubscription1 = this.demoService.person$
+      .pipe(
+        map((v) => v * v),
+        map((v) => v - 1),
+        take(4)
+      )
+      .subscribe(
+        (data) => console.log('Data 1', data),
+        (error) => console.error('Error 1', error),
+        () => console.log('Completed 1')
+      );
   }
 
   ngOnDestroy(): void {
     if (this.demoSubscription1) {
       this.demoSubscription1.unsubscribe();
-    }
-
-    if (this.demoSubscription2) {
-      this.demoSubscription2.unsubscribe();
-    }
-
-    if (this.demoSubscription3) {
-      this.demoSubscription3.unsubscribe();
     }
   }
 }
