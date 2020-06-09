89a096604e5705622f16b516b29a8466f79da972
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-09 22:30:20 +0530

    done

--- src/app/demo.service.ts
+++ src/app/demo.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Person {
+  name: string;
+  id: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DemoService {
+  personSubject = new BehaviorSubject<Person>({
+    name: 'Default User',
+    id: 101,
+  });
 
-  constructor() { }
+  constructor() {}
 }
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -9,5 +9,11 @@ import { DemoService } from '../demo.service';
 export class PlaygroundComponent implements OnInit {
   constructor(private demoService: DemoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.demoService.personSubject.subscribe(
+      (data) => console.log('Data', data),
+      (error) => console.error('Error', error),
+      () => console.log('Completed')
+    );
+  }
 }
