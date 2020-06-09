8f4296874d438681d40e8aefe833f7ffd6d8bfc1
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-09 22:34:51 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,33 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DemoService } from '../demo.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css'],
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, OnDestroy {
+  demoSubscription1: Subscription;
+  demoSubscription2: Subscription;
+  demoSubscription3: Subscription;
+
   constructor(private demoService: DemoService) {}
 
   ngOnInit(): void {
-    this.demoService.person$.subscribe(
+    this.demoSubscription1 = this.demoService.person$.subscribe(
       (data) => console.log('Data 1', data),
       (error) => console.error('Error 1', error),
       () => console.log('Completed 1')
     );
 
     // second subscriber
-    this.demoService.person$.subscribe(
+    this.demoSubscription2 = this.demoService.person$.subscribe(
       (data) => console.log('Data 2', data),
       (error) => console.error('Error 1', error),
       () => console.log('Completed 1')
     );
 
     //third
-    this.demoService.person$.subscribe(
+    this.demoSubscription3 = this.demoService.person$.subscribe(
       (data) => console.log('Data 3', data),
       (error) => console.error('Error 1', error),
       () => console.log('Completed 1')
     );
+  }
+
+  ngOnDestroy(): void {
+    if (this.demoSubscription1) {
+      this.demoSubscription1.unsubscribe();
+    }
+
+    if (this.demoSubscription2) {
+      this.demoSubscription2.unsubscribe();
+    }
+
+    if (this.demoSubscription3) {
+      this.demoSubscription3.unsubscribe();
+    }
   }
 }
